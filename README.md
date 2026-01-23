98a2d3c16ef437510d661cf3de9a09910a35ab99
Ben Burwood <ben.burwood@streetwave.co>
2026-01-23 21:16:51 +0000

    Implement Gather for Data Thinning

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Polars CLI
 
-The Polars CLI can be used for fairly rudimentary [Polars](https://pola.rs) 
+The Polars CLI can be used for fairly rudimentary [Polars](https://pola.rs) usage.
 
 ## Features
 
 - [X] Read/Write from CSV, Parquet, JSON
   - [X] Globbing
-  - [ ] S3 Cloud IO
+  - [ ] S3 Cloud IO - Requires `s3fs` Dependency
 - [X] Select Columns (comma seperated list)
 - [X] Drop Columns (comma seperated list)
+- [X] Gather for Thinning Datasets
 - [ ] Filter
+
+## Usage
+
+`polars-cli`
+
+`--input` - Input Filepath
+`--output` - Output Filepath
+`--drop` - Drop Columns (as 'Name,Age')
+`--select` - Select Columns (as 'Name,Age')
+`--gather` - Gather Every x Columns - Thin the Dataset
+`--lazy` - Execute Lazily (scan->sink)
--- main.py
+++ main.py
@@ -5,13 +5,19 @@ from polars_cli.io import input_file, output_file
 PARSER = argparse.ArgumentParser(prog="Polars CLI", description="Polars CLI is a command-line interface for the Polars DataFrame Library")
 PARSER.add_argument("-i", "--input", type=str, required=True, help="Input File Path - with Extension")
 PARSER.add_argument("-o", "--output", type=str, help="Output File Path - with Extension")
-PARSER.add_argument("-l", "--lazy", action="store_true", help="Use Lazy Evaluation")
 PARSER.add_argument("-s", "--select", type=str, help="List of Columns to Select (comma-seperated)")
 PARSER.add_argument("-d", "--drop", type=str, help="List of Columns to Drop (comma-seperated)")
+PARSER.add_argument("-g", "--gather", type=int, help="Gather Every Value - i.e. 3 means take every Third Row")
+PARSER.add_argument("--lazy", action="store_true", help="Use Lazy Evaluation")
 
 
-def main(input: str, output: str | None = None, lazy: bool = False, drop: str | None = None, select: str | None = None) -> None:
+def main(
+    input: str, output: str | None = None, drop: str | None = None, select: str | None = None, gather: int | None = None, lazy: bool = False
+) -> None:
     df = input_file(input, lazy=lazy)
+
+    if gather:
+        df.gather_every(gather)
 
     drop_list = set(drop.split(",")) if drop else None
     select_list = set(select.split(",")) if select else None
@@ -24,8 +30,6 @@ def main(input: str, output: str | None = None, lazy: bool = False, drop: str | 
         if select_list:
             df = df.select(select_list)
 
-    print(df)
-
     if output:
         output_file(df, output)
 
@@ -33,4 +37,4 @@ def main(input: str, output: str | None = None, lazy: bool = False, drop: str | 
 if __name__ == "__main__":
     args = PARSER.parse_args()
     print(args)
-    main(args.input, args.output, args.lazy, args.drop, args.select)
+    main(args.input, args.output, args.drop, args.select, args.gather, args.lazy)
